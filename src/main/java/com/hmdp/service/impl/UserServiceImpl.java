cc1f432f51ceb7b95f0acf17daa964c9d968b461
akalin <1494604032@qq.com>
2025-02-23 20:48:59 +0800

    发送验证码已完成

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,20 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.RedisConstants.LOGIN_CODE_KEY;
 
 /**
  * <p>
@@ -15,6 +25,24 @@ import org.springframework.stereotype.Service;
  * @since 2021-12-22
  */
 @Service
+@Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        //1.校验电话号
+        if(RegexUtils.isPhoneInvalid(phone)){
+            return Result.fail("手机号格式错误");
+        }
+        //2.生成6位数的code
+        String code = RandomUtil.randomNumbers(6);
+        String key=LOGIN_CODE_KEY+phone;
+        stringRedisTemplate.opsForValue().set(key,code);
+        //3.在idea控制台输出,模拟一下发送短信的功能
+        log.debug("发送短信验证码成功，验证码：{}", code);
+        return Result.ok();
+        //return Result.ok();
+    }
 }
--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,15 @@
 package com.hmdp;
 
+import com.hmdp.utils.RegexUtils;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
-
-
+    @Test
+    public void testPhone(){
+        System.out.println(RegexUtils.isPhoneInvalid("111"));
+        System.out.println(RegexUtils.isPhoneInvalid("13841121283"));
+        System.out.println(RegexUtils.isPhoneInvalid("12345678910"));
+    }
 }
